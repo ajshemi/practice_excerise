b7f1fd487664d8cfb9bf607c4d709cc21a3461fc
ajshemi <pojorshemi@gmail.com>
2019-01-10 14:01:06 -0500

    Added input checking.

--- function.py
+++ function.py
@@ -11,6 +11,12 @@ def f(x):
     =======
     y: float
         (positive) output number
+    
+    Raises
+    ======
+    ValueError if x is neither int or float
     '''
+    if type(x) not in [int,float]:
+        raise ValueError('Parameter must be integer or float.')
     y = x ** 2
     return y
